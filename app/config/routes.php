047c54dde466c045eb2c40d84a29c0a0d6647516
Princess Winlyn Yu <princesswinlynyu@gmail.com>
2026-09-14 22:36:56 +0800

    Fixed routes

--- app/config/routes.php
+++ app/config/routes.php
@@ -3,14 +3,41 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 /** @var object $router **/
 
+/*
+|--------------------------------------------------------------------------
+| HOME
+|--------------------------------------------------------------------------
+*/
+
 $router->get('/', 'HomeController::index');
+
+
+/*
+|--------------------------------------------------------------------------
+| STUDENT
+|--------------------------------------------------------------------------
+*/
 
 $router->get('/student', 'StudentController::index');
 
 $router->get('/student/profile', 'StudentController::profile')
        ->middleware('student');
 
+
+/*
+|--------------------------------------------------------------------------
+| USERS TABLE
+|--------------------------------------------------------------------------
+*/
+
 $router->get('/users', 'UsersController::index');
+
+
+/*
+|--------------------------------------------------------------------------
+| LAB 4 - CRUD USERS
+|--------------------------------------------------------------------------
+*/
 
 $router->get('/crud', 'CrudController::index');
 
@@ -24,48 +51,40 @@ $router->post('/crud/update/{id}', 'CrudController::update');
 
 $router->get('/crud/delete/{id}', 'CrudController::delete');
 
+
+/*
+|--------------------------------------------------------------------------
+| LAB 5 - LOGIN & SIGNUP
+|--------------------------------------------------------------------------
+*/
+
 $router->get('/lab5', 'AuthController::index');
 
 $router->get('/signup', 'AuthController::signup');
+
 $router->post('/signup/store', 'AuthController::store');
 
 $router->get('/login', 'AuthController::login');
+
 $router->post('/login/authenticate', 'AuthController::authenticate');
 
 $router->get('/logout', 'AuthController::logout');
 
-$router->get('/products', 'ProductController::index')
-       ->middleware('auth');
 
-$router->get('/products/create', 'ProductController::create')
-       ->middleware('auth');
-
-$router->post('/products/store', 'ProductController::store')
-       ->middleware('auth');
-
-$router->get('/products/edit/{id}', 'ProductController::edit')
-       ->middleware('auth');
-
-$router->post('/products/update/{id}', 'ProductController::update')
-       ->middleware('auth');
-
-$router->get('/products/delete/{id}', 'ProductController::delete')
-       ->middleware('auth');
-       
-$router->get('/lab5', 'AuthController::index');
-
-$router->get('/login', 'AuthController::login');
-
-$router->get('/signup', 'AuthController::signup');     
-
-$router->post('/signup/store', 'AuthController::store');
-
-$router->post('/login/authenticate', 'AuthController::authenticate');
-
-$router->get('/logout', 'AuthController::logout');
+/*
+|--------------------------------------------------------------------------
+| PRODUCTS CRUD
+|--------------------------------------------------------------------------
+*/
 
 $router->get('/products', 'ProductController::index');
 
 $router->get('/products/create', 'ProductController::create');
 
 $router->post('/products/store', 'ProductController::store');
+
+$router->get('/products/edit/{id}', 'ProductController::edit');
+
+$router->post('/products/update/{id}', 'ProductController::update');
+
+$router->get('/products/delete/{id}', 'ProductController::delete');
